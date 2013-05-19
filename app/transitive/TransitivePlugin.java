74c74acd9a1764d43557c7d3c71b7e7625035551
André Pareis <andre@pareis.com>
2013-05-19 19:07:18 +0200

    renamed folders and files to my liking

--- app/transitive/TransitivePlugin.java
+++ app/transitive/TransitivePlugin.java
@@ -18,7 +18,7 @@ public class TransitivePlugin extends PlayPlugin {
         public void enhanceThisClass(ApplicationClasses.ApplicationClass applicationClass) throws Exception {
             CtClass ctClass = makeClass(applicationClass);
 
-            if (!ctClass.subtypeOf(classPool.get("alterbase.GenericTransitiveModel"))) {
+            if (!ctClass.subtypeOf(classPool.get("transitive.GenericTransitiveModel"))) {
                 return;
             }
 
@@ -30,43 +30,43 @@ public class TransitivePlugin extends PlayPlugin {
             String entityName = ctClass.getName();
 
             // count
-            CtMethod count = CtMethod.make("public static long count() { return alterbase.jpa.JPQL.instance.count(\"" + entityName + "\"); }", ctClass);
+            CtMethod count = CtMethod.make("public static long count() { return transitive.jpa.JPQL.instance.count(\"" + entityName + "\"); }", ctClass);
             ctClass.addMethod(count);
 
             // count2
-            CtMethod count2 = CtMethod.make("public static long count(String query, Object[] params) { return alterbase.jpa.JPQL.instance.count(\"" + entityName + "\", query, params); }", ctClass);
+            CtMethod count2 = CtMethod.make("public static long count(String query, Object[] params) { return transitive.jpa.JPQL.instance.count(\"" + entityName + "\", query, params); }", ctClass);
             ctClass.addMethod(count2);
 
             // findAll
-            CtMethod findAll = CtMethod.make("public static java.util.List findAll() { return alterbase.jpa.JPQL.instance.findAll(\"" + entityName + "\"); }", ctClass);
+            CtMethod findAll = CtMethod.make("public static java.util.List findAll() { return transitive.jpa.JPQL.instance.findAll(\"" + entityName + "\"); }", ctClass);
             ctClass.addMethod(findAll);
 
             // findById
-            CtMethod findById = CtMethod.make("public static alterbase.GenericTransitiveModel findById(Object id) { return alterbase.jpa.JPQL.instance.findById(\"" + entityName + "\", id); }", ctClass);
+            CtMethod findById = CtMethod.make("public static transitive.GenericTransitiveModel findById(Object id) { return transitive.jpa.JPQL.instance.findById(\"" + entityName + "\", id); }", ctClass);
             ctClass.addMethod(findById);
 
             // find
-            CtMethod find = CtMethod.make("public static alterbase.GenericTransitiveModel.JPAQuery find(String query, Object[] params) { return alterbase.jpa.JPQL.instance.find(\"" + entityName + "\", query, params); }", ctClass);
+            CtMethod find = CtMethod.make("public static transitive.GenericTransitiveModel.JPAQuery find(String query, Object[] params) { return transitive.jpa.JPQL.instance.find(\"" + entityName + "\", query, params); }", ctClass);
             ctClass.addMethod(find);
 
             // find
-            CtMethod find2 = CtMethod.make("public static alterbase.GenericTransitiveModel.JPAQuery find() { return alterbase.jpa.JPQL.instance.find(\"" + entityName + "\"); }", ctClass);
+            CtMethod find2 = CtMethod.make("public static transitive.GenericTransitiveModel.JPAQuery find() { return transitive.jpa.JPQL.instance.find(\"" + entityName + "\"); }", ctClass);
             ctClass.addMethod(find2);
 
             // all
-            CtMethod all = CtMethod.make("public static alterbase.GenericTransitiveModel.JPAQuery all() { return alterbase.jpa.JPQL.instance.all(\"" + entityName + "\"); }", ctClass);
+            CtMethod all = CtMethod.make("public static transitive.GenericTransitiveModel.JPAQuery all() { return transitive.jpa.JPQL.instance.all(\"" + entityName + "\"); }", ctClass);
             ctClass.addMethod(all);
 
             // delete
-            CtMethod delete = CtMethod.make("public static int delete(String query, Object[] params) { return alterbase.jpa.JPQL.instance.delete(\"" + entityName + "\", query, params); }", ctClass);
+            CtMethod delete = CtMethod.make("public static int delete(String query, Object[] params) { return transitive.jpa.JPQL.instance.delete(\"" + entityName + "\", query, params); }", ctClass);
             ctClass.addMethod(delete);
 
             // deleteAll
-            CtMethod deleteAll = CtMethod.make("public static int deleteAll() { return alterbase.jpa.JPQL.instance.deleteAll(\"" + entityName + "\"); }", ctClass);
+            CtMethod deleteAll = CtMethod.make("public static int deleteAll() { return transitive.jpa.JPQL.instance.deleteAll(\"" + entityName + "\"); }", ctClass);
             ctClass.addMethod(deleteAll);
 
             // findOneBy
-            CtMethod findOneBy = CtMethod.make("public static alterbase.GenericTransitiveModel findOneBy(String query, Object[] params) { return alterbase.jpa.JPQL.instance.findOneBy(\"" + entityName + "\", query, params); }", ctClass);
+            CtMethod findOneBy = CtMethod.make("public static transitive.GenericTransitiveModel findOneBy(String query, Object[] params) { return transitive.jpa.JPQL.instance.findOneBy(\"" + entityName + "\", query, params); }", ctClass);
             ctClass.addMethod(findOneBy);
 
 
